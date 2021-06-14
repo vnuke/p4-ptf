66207df45140cc8d9a67d5328c0d2e5eee8f7841
VincentBrouillard <vincent.brouillard@protonmail.com>
2021-06-14 15:39:49 -0400

    using bmpy_utils

--- mytests/sai_base_test.py
+++ mytests/sai_base_test.py
@@ -22,64 +22,15 @@ import bm_runtime
 from bm_runtime.standard.Standard import Client
 from bm_runtime.standard.ttypes import *
 
-from thrift.transport import TSocket
-from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
+import bmpy_utils
+
 from thrift.Thrift import TException, TApplicationException
-
-class SAIThriftTest(BaseTest):
-
-    def setUp(self):
-        BaseTest.setUp(self)
-
-        test_params = testutils.test_params_get()
-        print("You specified the following test-params when invoking ptf:")
-        for k, v in test_params.items():
-            print(k, ":\t\t\t", v)
-
-        # Set up thrift client and contact server
-        self.transport = TSocket.TSocket('localhost', 9090)
-        self.transport = TTransport.TBufferedTransport(self.transport)
-        self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
-
-        self.client = Client(self.protocol)
-        self.transport.open()
-
-    def tearDown(self):
-        BaseTest.tearDown(self)
-        self.transport.close()
-
-
-class SAIThriftDataplaneTest(SAIThriftTest):
-
-    def setUp(self):
-        SAIThriftTest.setUp(self)
-
-        # shows how to use a filter on all our tests
-        testutils.add_filter(testutils.not_ipv6_filter)
-
-        self.dataplane = ptf.dataplane_instance
-        self.dataplane.flush()
-        if config["log_dir"] != None:
-            filename = os.path.join(config["log_dir"], str(self)) + ".pcap"
-            self.dataplane.start_pcap(filename)
-
-    def tearDown(self):
-        if config["log_dir"] != None:
-            self.dataplane.stop_pcap()
-        testutils.reset_filters()
-        SAIThriftTest.tearDown(self)
 
 
 class AGFBaseTest(BaseTest):
     def setUp(self):
         BaseTest.setUp(self)
-        self.transport = TSocket.TSocket('localhost', 9090)
-        self.transport = TTransport.TBufferedTransport(self.transport)
-        self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
-
-        self.client = Client(self.protocol)
-        self.transport.open()
+        bmpy_utils.thrift_connect_standard('localhost', 9090)
 
         self.context = 1
 
