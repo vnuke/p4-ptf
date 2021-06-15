c469ccae4942c0dbca27c4967d30c027bf014226
VincentBrouillard <vincent.brouillard@protonmail.com>
2021-06-14 21:33:01 -0400

    remplacement de la fonction bytes_to_string de thriftutils par celle de runtimeCLI

--- mytests/sai_base_test.py
+++ mytests/sai_base_test.py
@@ -71,7 +71,7 @@ class AGFBaseTest(BaseTest):
         try:
             print(self.standard_client.bm_mgmt_get_info())
 
-            lpm_param = BmMatchParamLPM(key=thriftutils.bytes_to_string(self.ip_rule),
+            lpm_param = BmMatchParamLPM(key=bytes_to_string(self.ip_rule),
                                         prefix_length=self.prefix_len_rule)
             param = BmMatchParam(type=BmMatchParamType.LPM, lpm=lpm_param)
 
